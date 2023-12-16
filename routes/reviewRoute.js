6f1e4975ad8262ba621a1bdaa29abac6310679f4
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-12-17 03:40:33 +0700

    Add files via upload

---
+++ routes/reviewRoute.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const reviewController = require('../controllers/reviewController')
+const authMiddleware = require('../middlewares/authentication')
+const authorizationMiddleware = require('../middlewares/authorization') 
+
+router.get('/', reviewController.findAll) 
+
+router.get('/:id', reviewController.findById)
+
+router.post('/', 
+  authMiddleware,
+  reviewController.create) 
+  
+router.put('/:id', 
+  authMiddleware,
+  authorizationMiddleware(['owner', 'admin']), 
+  reviewController.update)
+
+router.delete('/:id', 
+  authMiddleware,
+  authorizationMiddleware(['owner', 'admin']),
+  reviewController.delete)
+
+module.exports = router
